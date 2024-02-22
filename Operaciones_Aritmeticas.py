8cd9716c9f20ebd9cdb015f8f30b7940cc6f7c6e
“MasterRuiz15” <andresruiz@colegioguanenta.edu.co>
2024-02-22 16:00:17 -0500

    Se realizo Analisis y Diseño

---
+++ Operaciones_Aritmeticas.py
@@ -0,0 +1,23 @@
+# input 
+
+X = int(input("Por favor digite un numero : "))
+Y = int(input("Por favor digite un numero : "))
+
+# processing
+S = (X+Y)
+R = (X-Y)
+M = (X*Y)
+D = (X/Y)
+DE = (X//Y)
+MOD = (X%Y)
+P = (X**Y)
+
+# output
+print(" Vamos aver los resultados de las operaciones:")
+print("suma:",S)
+print("resta:",R)
+print("multiplicacion:" ,M)
+print("division:" ,D)
+print("division entera:" ,DE)
+print("modulo:" ,MOD)
+print("potencia:" ,P)
